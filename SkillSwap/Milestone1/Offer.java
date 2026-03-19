044c9d99c1f53c738cb224dba26761f3dbbdf2a3
S8289503J <s8289503j@localhost.localdomain>
2026-03-19 17:37:54 +0100

    Ho impostato tutto pronto da continuare a casa

--- SkillSwap/Milestone1/Offer.java
+++ SkillSwap/Milestone1/Offer.java
@@ -1,14 +1,16 @@
 package SkillSwap.Milestone1;
 
-public class Offer {
+import SkillSwap.Milestone2.*;
+
+    public class Offer {
     private String id;
     private Student student;
     private Skill skill;
-    private String level;
+    private Level level;
     private String note;
     private boolean active;
 
-    public Offer(String id, Student student, Skill skill, String level, String note) {
+    public Offer(String id, Student student, Skill skill, Level level, String note) {
         this.id = id;
         this.student = student;
         this.skill = skill;
@@ -17,6 +19,7 @@ public class Offer {
         this.active = true;
     }
 
+    public Level getLevel() { return level; }
     public String getId() { return id; }
     public Student getStudent() { return student; }
     public Skill getSkill() { return skill; }
