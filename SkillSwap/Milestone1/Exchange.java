0e30d33c4af2ed780956c56a7ce337d2e1c0743e
S8289503J <s8289503j@localhost.localdomain>
2026-03-19 15:35:17 +0100

    Exchange.java [NON TOCCARE PER ORA]

---
+++ SkillSwap/Milestone1/Exchange.java
@@ -0,0 +1,15 @@
+package SkillSwap.Milestone1;
+
+public class Exchange {
+    private String id;
+    private Offer offer;
+    private Request request;
+    private String status;
+
+    public Exchange(String id, Offer offer, Request request) {
+        this.id = id;
+        this.offer = offer;
+        this.request = request;
+        this.status = "PROPOSED";
+    }
+}
