94aef91e0336dd8cea772b318a0267da5080ea25
S8289503J <s8289503j@localhost.localdomain>
2026-03-19 16:39:15 +0100

    Ho messo il main.java [COMPILA]

---
+++ SkillSwap/Milestone1/SkillSwapState.java
@@ -0,0 +1,10 @@
+package SkillSwap.Milestone1;
+
+import java.util.*;
+
+public class SkillSwapState {
+    public Map<String, Student> students = new HashMap<>();
+    public Map<String, Skill> skills = new HashMap<>();
+    public Map<String, Offer> offers = new HashMap<>();
+    public Map<String, Request> requests = new HashMap<>();
+}
